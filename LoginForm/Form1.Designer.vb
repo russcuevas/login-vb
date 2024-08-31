0559ba68f2c1d7dd45ae65535735c05cb72e8e72
Russ <russelcuevas0@gmail.com>
2024-08-31 11:13:37 +0800

    vb-net-login

---
+++ LoginForm/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lblmessage = New System.Windows.Forms.Label()
+        Me.MsgAttempt = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.Location = New System.Drawing.Point(72, 51)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(55, 13)
+        Me.UsernameLabel.TabIndex = 1
+        Me.UsernameLabel.Text = "Username"
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.AutoSize = True
+        Me.PasswordLabel.Location = New System.Drawing.Point(72, 84)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(53, 13)
+        Me.PasswordLabel.TabIndex = 2
+        Me.PasswordLabel.Text = "Password"
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(285, 62)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(105, 26)
+        Me.LoginButton.TabIndex = 3
+        Me.LoginButton.Text = "Login"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(135, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(104, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(135, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(104, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'lblmessage
+        '
+        Me.lblmessage.AutoSize = True
+        Me.lblmessage.Location = New System.Drawing.Point(72, 118)
+        Me.lblmessage.Name = "lblmessage"
+        Me.lblmessage.Size = New System.Drawing.Size(50, 13)
+        Me.lblmessage.TabIndex = 6
+        Me.lblmessage.Text = "Message"
+        '
+        'MsgAttempt
+        '
+        Me.MsgAttempt.AutoSize = True
+        Me.MsgAttempt.Location = New System.Drawing.Point(282, 134)
+        Me.MsgAttempt.Name = "MsgAttempt"
+        Me.MsgAttempt.Size = New System.Drawing.Size(48, 13)
+        Me.MsgAttempt.TabIndex = 7
+        Me.MsgAttempt.Text = "Attempts"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(448, 172)
+        Me.Controls.Add(Me.MsgAttempt)
+        Me.Controls.Add(Me.lblmessage)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents LoginButton As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblmessage As System.Windows.Forms.Label
+    Friend WithEvents MsgAttempt As System.Windows.Forms.Label
+
+End Class
---
+++ LoginForm/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Dim userName As String
+    Dim password As String
+    Dim retry As Integer = 0
+    Dim process As Boolean
+    Dim MaxAttempts As Integer = 5
+
+    Private Sub LoginButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoginButton.Click
+
+        userName = TextBox1.Text
+        password = TextBox2.Text
+
+        process = True
+        retry = retry + 1
+
+        While process = True
+            If userName = "Russel" And password = "hello" Then
+                lblmessage.Text = "Welcome " & userName
+                process = False
+                retry = 0
+                MsgAttempt.Text = ""
+            Else
+                MsgBox("Wrong Information, Please Try Again")
+                MsgAttempt.Text = "Attempts: " & (MaxAttempts - retry) & " attempts remaining"
+                process = False
+            End If
+
+            If retry = MaxAttempts Then
+                LoginButton.Enabled = False
+                MsgBox("Sorry your account is locked out")
+                MsgAttempt.Text = "Sorry, your account is locked out"
+            End If
+
+        End While
+    End Sub
+End Class
